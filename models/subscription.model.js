285065d5461c0c0e58a46cc7ac2a0163318636bf
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-02-22 23:25:43 +0000

    working on the subscription model

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -27,3 +27,7 @@ const subscriptionSchema = new mongoose.Schema({
     }
 
 }, { timestamps: true });
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema);
+
+export default Subscription

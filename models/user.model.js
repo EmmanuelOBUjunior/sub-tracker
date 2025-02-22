a159c55b2a1b563eab7d39871ad6704f60fa3943
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-02-22 23:18:47 +0000

    working on user model

--- models/user.model.js
+++ models/user.model.js
@@ -12,6 +12,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Email is required"],
     trim: true,
+    unique: true,
+    lowercase: true,
     minLength: 4,
     maxLength: 255,
     match: [/\S+@\S+\.\S+/, "Please enter a valid email address"]

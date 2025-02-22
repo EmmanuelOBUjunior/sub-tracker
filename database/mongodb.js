7e838856fff14ac8cdcd2af05b342958480562d1
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-02-22 22:51:11 +0000

    working on the database setup

---
+++ database/mongodb.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_URI, NODE_ENV } from "../config/env";
+
+if (!DB_URI) {
+  throw new Error(
+    "Please define the mongodb_uri environment variable inside the env<development||production>.local"
+  );
+}
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(DB_URI)
+    console.log(`Connected to database in ${NODE_ENV} mode`);
+  } catch (error) {
+    console.log("Error connecting to database: ", error);
+    process.exit(1);
+  }
+};
+
+export default connectToDatabase;

30d54a335e7846feb0a05952d0bd724b865ce475
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-02-22 21:39:50 +0000

    Setting up structure for user routes

---
+++ routes/user.route.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+const userRouter = Router()
+
+userRouter.get('/', (req, res)=> res.send({title: 'Get all users'}))
+userRouter.get('/:id', (req, res)=> res.send({title: 'Get user details'}))
+userRouter.post('/', (req, res)=> res.send({title: 'Create new user'}))
+userRouter.put('/:id', (req, res)=> res.send({title: 'Update a user'}))
+userRouter.delete('/', (req, res)=> res.send({title: 'Delete user'}))
+
+
+export default userRouter

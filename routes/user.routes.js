5d6ce462e90d41508e03ecd92464296e3a8a7c7a
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-02-23 17:14:17 +0000

    working on the authorization middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getUser, getUsers } from "../contollers/users.controller.js";
+import authorize from "../middleware/authorization.middleware.js";
 
 const userRouter = Router()
 
 userRouter.get('/', getUsers)
-userRouter.get('/:id', getUser)
+userRouter.get('/:id',authorize,getUser)
 userRouter.post('/', (req, res)=> res.send({title: 'Create new user'}))
 userRouter.put('/:id', (req, res)=> res.send({title: 'Update a user'}))
 userRouter.delete('/', (req, res)=> res.send({title: 'Delete user'}))

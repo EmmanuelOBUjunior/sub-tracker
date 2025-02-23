db14a8beacad69b5839c5207fe0f9c2b2abacad6
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-02-23 17:07:57 +0000

    working on the authorization middleware

--- middleware/authorization.middleware.js
+++ middleware/authorization.middleware.js
@@ -1,6 +1,21 @@
+
+
 const authorize = (req, res, next) => {
     try {
-        
+        let token
+        if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
+            token = req.headers.authorization.split(' ')[1]
+        }
+
+        if(!token){
+            res.status(401).json({
+                success: false,
+                message: "Unauthorized"
+            })
+        }
+
+        const decoded = 
+
     } catch (error) {
         res.status(401).json({
             success: false,

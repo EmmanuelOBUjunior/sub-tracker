dbc62430676327bf70a78489098cf7dda55e908f
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-02-23 12:29:44 +0000

    working on users controller

---
+++ contollers/users.controller.js
@@ -0,0 +1,13 @@
+import User from "../models/user.model"
+
+export const getUsers = async (req, res, next) => {
+    try {
+        const users =  await User.find()
+        res.status(200).json({
+            success: true,
+            data: users
+        })
+    } catch (error) {
+        next(error)
+    }
+}

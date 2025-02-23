81f1a893f4dbf580f916b60027d8b7cbd3948e4a
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-02-23 00:19:32 +0000

    working on error middleware

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -4,11 +4,20 @@ const errorMiddleware = (err, req, res, next) => {
     error.message = err.message
     console.log(err)
 
+    //Mongoose bad ObjectId
     if(err.name === "CastError"){
         const message = `Resource not found. Invalid: ${err.path}`
         error = new Error(message)
         error.statusCode = 404
     }
+
+    //Mongoose duplicate key
+    if(err.code === 11000){
+        const message = `Duplicate field value entered`
+        error = new Error(message)
+        error.statusCode = 400
+    }
+    
     }catch(error){
         console.log(error)
         next(error)

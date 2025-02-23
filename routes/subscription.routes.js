5e38568a9bfba2ea23d01e81d97ffec0114b0081
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-02-23 21:48:02 +0000

    working on the subscription controller

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import authorize from "../middleware/authorization.middleware.js";
-import { createSubscription } from "../contollers/subscription.controller.js";
+import { createSubscription, getUserSubscriptions } from "../contollers/subscription.controller.js";
 
 const subscriptionRouter = Router()
 
 subscriptionRouter.get('/', (req,res)=> res.send({title:'Get all subscriptions'}))
 subscriptionRouter.get('/:id', (req,res)=> res.send({title:'Get subscription details'}))
-subscriptionRouter.get('/user/:id', (req,res)=> res.send({title:'Get all subscriptions'}))
+subscriptionRouter.get('/user/:id',authorize, getUserSubscriptions)
 subscriptionRouter.post('/', authorize, createSubscription)
 subscriptionRouter.put('/:id', (req,res)=> res.send({title:'Update a subscription'}))
 subscriptionRouter.delete('/:id', (req,res)=> res.send({title:'Delete a subscription'}))

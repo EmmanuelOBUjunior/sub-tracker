f4fff36b77707c6adab3e164e2210989355bf692
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-02-23 20:53:17 +0000

    working on the arcjet middleware

--- config/arcjet.js
+++ config/arcjet.js
@@ -5,21 +5,13 @@ const aj = arcjet({
     key: ARCJET_KEY,
     characteristics: ["ip.src"], // Track requests by IP
     rules: [
-      // Shield protects your app from common attacks e.g. SQL injection
       shield({ mode: "LIVE" }),
-      // Create a bot detection rule
       detectBot({
-        mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
-        // Block all bots except the following
+        mode: "LIVE", 
         allow: [
-          "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-          // Uncomment to allow these other common bot categories
-          // See the full list at https://arcjet.com/bot-list
-          //"CATEGORY:MONITOR", // Uptime monitoring services
-          //"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
+          "CATEGORY:SEARCH_ENGINE", 
         ],
       }),
-      // Create a token bucket rate limit. Other algorithms are supported.
       tokenBucket({
         mode: "LIVE",
         refillRate: 5, // Refill 5 tokens per interval

25b56a87b1c9d40c699053fb14dc0b1abf8b273e
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-02-23 20:38:03 +0000

    working on the arcjet middleware

--- middleware/arcjet.middleware.js
+++ middleware/arcjet.middleware.js
@@ -1,1 +1,16 @@
 import { aj } from "../config/arcjet.js";
+
+export const arcjectMiddleware = async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req);
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit)
+        res.status(429).json({ message: "Too many requests" });
+      if (decision.reason.isBot)
+        res.status(403).json({ message: "Bot detected" });
+    }
+  } catch (error) {
+    console.log(`Arcjet Middleware Error: ${error.message}`);
+    next(error);
+  }
+};

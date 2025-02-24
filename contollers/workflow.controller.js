778a74040e82eb8048ea6c9f948183e7a9b8303c
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-02-24 00:46:45 +0000

    working on sendremainder controller

--- contollers/workflow.controller.js
+++ contollers/workflow.controller.js
@@ -2,4 +2,6 @@ import {createRequire} from 'module'
 const require = createRequire(import.meta.url)
 const {serve} = require('@upstash/workflow/express')
 
-export const sendRemainders = serve
+export const sendRemainders = serve(async(context)=>{
+    
+})

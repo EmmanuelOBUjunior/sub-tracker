9e0fd0ee378e7bdbc75aa5d7a9bf44d049100e8a
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-02-24 00:06:52 +0000

    working on workflow api

--- config/qstash.js
+++ config/qstash.js
@@ -1,6 +1,7 @@
 import {Client as WorkflowClient} from '@upstash/workflow';
-import { QSTASH_URL } from './env.js';
+import { QSTASH_TOKEN, QSTASH_URL } from './env.js';
 
 export const workflow = new WorkflowClient({
-    baseUrl: QSTASH_URL
+    baseUrl: QSTASH_URL,
+    token: QSTASH_TOKEN
 })

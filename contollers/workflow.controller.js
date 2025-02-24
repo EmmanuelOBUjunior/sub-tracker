4da1f112db84eb15e134cbb5c238f9e254ba458f
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-02-24 09:27:04 +0000

    working on sendremainder controller

--- contollers/workflow.controller.js
+++ contollers/workflow.controller.js
@@ -4,6 +4,8 @@ import dayjs from 'dayjs'
 const require = createRequire(import.meta.url)
 const {serve} = require('@upstash/workflow/express')
 
+const REMAINDERS = [7,5,2,1]
+
 export const sendRemainders = serve(async(context)=>{
     const {subscriptionId} = context.requestPayload
     const subscription = await fetchSubscription(context, subscriptionId)
@@ -17,10 +19,23 @@ export const sendRemainders = serve(async(context)=>{
     if(renewalDate.isBefore(dayjs())){
         console.log(`Renewal date has passed for subscription ${subscriptionId}. Stopping workflow!`)
     }
+
+    for(const daysbefore of REMAINDERS){
+        const remainderDate = renewalDate.subtract(daysbefore, 'day')
+
+        if(remainderDate.isAfter(dayjs)){
+            sleepUntilReminder
+        }
+    }
+
 })
 
 const fetchSubscription = async(context, subscriptionId)=>{
     return await context.run('get subscription', ()=>{
         return Subscription.findById(subscriptionId).populate('user').select('name email')
     })
+}
+
+const sleepUntilReminder = ()=>{
+
 }

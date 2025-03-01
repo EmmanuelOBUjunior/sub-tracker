8583160c4128896f8a78d6beee399ecd9d48d23b
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-01 09:51:31 +0000

    working on send reminder controller

--- contollers/subscription.controller.js
+++ contollers/subscription.controller.js
@@ -1,3 +1,5 @@
+import { SERVER_URL } from "../config/env.js"
+import { workflowClient } from "../config/qstash.js"
 import Subscription from "../models/subscription.model.js"
 
 export const createSubscription = async(req,res, next)=>{
@@ -7,9 +9,20 @@ export const createSubscription = async(req,res, next)=>{
             user:req.user._id
         })
 
+        const {worflowRunId} = await workflowClient.trigger({
+            url:`${SERVER_URL}/api/v1/subscriptions/reminder`,
+            body:{
+                subscriptionId: subscription.id
+            },
+            headers:{
+                'Content-Type':'application/json'
+            },
+            retries: 0
+        })
+
         res.status(201).json({
             success: true,
-            data: subscription
+            data: {subscription, worflowRunId}
         })
     } catch (error) {
         next(error)

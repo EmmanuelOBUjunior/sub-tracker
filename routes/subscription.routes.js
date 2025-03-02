3df01d4fc3b7e052f0f9ceaab4f25dd68528ddc8
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-02 21:26:13 +0000

    feat:logic for updating a subscription

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import authorize from "../middleware/authorization.middleware.js";
-import { createSubscription, getSubscriptions, getUserSubscriptions } from "../contollers/subscription.controller.js";
+import { createSubscription, getSubscriptions, getUserSubscriptions, updateSubscription } from "../contollers/subscription.controller.js";
 
 const subscriptionRouter = Router()
 
@@ -8,7 +8,7 @@ subscriptionRouter.get('/', getSubscriptions)
 subscriptionRouter.get('/:id', (req,res)=> res.send({title:'Get subscription details'}))
 subscriptionRouter.get('/user/:id',authorize, getUserSubscriptions)
 subscriptionRouter.post('/', authorize, createSubscription)
-subscriptionRouter.put('/:id', (req,res)=> res.send({title:'Update a subscription'}))
+subscriptionRouter.put('/:id', updateSubscription)
 subscriptionRouter.delete('/:id', (req,res)=> res.send({title:'Delete a subscription'}))
 subscriptionRouter.put('/:id/cancel', (req,res)=> res.send({title:'Cancel subscription'}))
 subscriptionRouter.get('/upcoming-renewals', (req,res)=> res.send({title:'Get all upcoming renewals'}))

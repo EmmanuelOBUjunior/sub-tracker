d29f49ea618467ff5e670d3a7792a564609883cd
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-02 19:46:31 +0000

    working on the logic to send email

--- utils/send-email.js
+++ utils/send-email.js
@@ -3,5 +3,9 @@ import { emailTemplates } from "./email-template.js"
 export const sendReminderEmail = async({to, type, subscription}) =>{
 if(!to||!type) throw new Error("Missing required parameters!!")
 
-const template = emailTemplates
+const template = emailTemplates.find((t)=> t.label === type)
+
+if(!template) throw new Error("Invalid email type")
+
+
 }

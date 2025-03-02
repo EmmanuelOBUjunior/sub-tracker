07aa7a7eeeb659f0c5c6cbb5690947dc88980f01
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-02 19:55:06 +0000

    working on the logic to send email

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -3,7 +3,7 @@ import { EMAIL_PASSWORD } from './env.js'
 
 export const accountEmail = 'obujuniperjunior1998@gmail.com'
 
-const transporter = nodemailer.createTransport({
+export const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth:{
         user: accountEmail,

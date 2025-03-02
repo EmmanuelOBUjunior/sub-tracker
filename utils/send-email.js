b8ed789e6a002f3e37926c9d87868df753417a96
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-02 19:51:22 +0000

    working on the logic to send email

--- utils/send-email.js
+++ utils/send-email.js
@@ -1,18 +1,19 @@
-import dayjs from "dayjs"
-import { emailTemplates } from "./email-template.js"
+import dayjs from "dayjs";
+import { emailTemplates } from "./email-template.js";
 
-export const sendReminderEmail = async({to, type, subscription}) =>{
-if(!to||!type) throw new Error("Missing required parameters!!")
+export const sendReminderEmail = async ({ to, type, subscription }) => {
+  if (!to || !type) throw new Error("Missing required parameters!!");
 
-const template = emailTemplates.find((t)=> t.label === type)
+  const template = emailTemplates.find((t) => t.label === type);
 
-if(!template) throw new Error("Invalid email type")
+  if (!template) throw new Error("Invalid email type");
 
-const mailInfo = {
+  const mailInfo = {
     userName: subscription.user.name,
     subscriptionName: subscription.name,
-    renewalDate: dayjs(subscription.renewalDate).format('MMM D, YYYY'),
+    renewalDate: dayjs(subscription.renewalDate).format("MMM D, YYYY"),
     planName: subscription.name,
     price: `${subscription.currency} ${subscription.price} ${subscription.frequency}`,
-}
-}
+    paymentMethod: subscription.paymentMethod,
+  };
+};
